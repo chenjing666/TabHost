671f5663cec2efc79826435dc9caa5bc9d24b5d4
hasee <1774383254@qq.com>
2017-02-23 16:40:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hasee/tabhost/AddExamActivity.java
@@ -0,0 +1,15 @@
+package com.example.hasee.tabhost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hasee on 2017/2/23.
+ */
+public class AddExamActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all);
+    }
+}
---
+++ app/src/main/java/com/example/hasee/tabhost/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.example.hasee.tabhost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hasee on 2017/2/23.
+ */
+public class SettingActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alll);
+    }
+}
